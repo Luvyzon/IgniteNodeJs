a22de3902654b13d8866acc633b543385d092b9d
Mateus Magro <75760729+MateusMgr@users.noreply.github.com>
2023-02-10 18:15:44 -0300

    Update server.js

--- 01-fundamentos node/src/server.js
+++ 01-fundamentos node/src/server.js
@@ -2,6 +2,9 @@ import http, { createServer } from 'node:http'
 
 const server = http.createServer((req, res) => {
     return res.end('hello world')
+    
+    console.log(return)
+    
 })
 
 server.listen(3333)
